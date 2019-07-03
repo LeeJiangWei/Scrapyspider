e2d77776e0b703b18b615d053b6e6655642f457b
Lee Jiangwei <ljwskrskr@outlook.com>
2019-07-03 23:01:35 +0800

    optimize arkwiki_spider, output single value rather than array

--- scrapyspider/spiders/arkwiki_spider.py
+++ scrapyspider/spiders/arkwiki_spider.py
@@ -18,25 +18,26 @@ class ArkWikiSpider(Spider):
         operators = response.xpath('//table[@id="CardSelectTr"]/tr')
         # print("operators:", operators)
         for operator in operators:
-            item['name_zh'] = operator.xpath(
-                './td[2]/a/text()'
-            ).extract()
-            item['sex'] = operator.xpath(
-                './@data-param3'
-            ).extract()
-            item['operator_class'] = operator.xpath(
-                './@data-param1'
-            ).extract()
-            item['stars'] = operator.xpath(
-                './@data-param2'
-            ).re(r'[1-6]')
-            item['tags'] = operator.xpath(
-                './@data-param5'
-            ).extract()
-            item['img_src'] = operator.xpath(
-                './td[1]/div/div/a/img/@src'
-            ).extract()
-            yield item
+            if len(operator.xpath('./td[2]/a/text()').extract()) != 0:
+                item['name_zh'] = operator.xpath(
+                    './td[2]/a/text()'
+                ).extract()[0]
+                item['sex'] = operator.xpath(
+                    './@data-param3'
+                ).extract()[0]
+                item['operator_class'] = operator.xpath(
+                    './@data-param1'
+                ).extract()[0]
+                item['stars'] = operator.xpath(
+                    './@data-param2'
+                ).re(r'[1-6]')[0]
+                item['tags'] = operator.xpath(
+                    './@data-param5'
+                ).extract()[0]
+                item['img_src'] = operator.xpath(
+                    './td[1]/div/div/a/img/@src'
+                ).extract()[0]
+                yield item
 
 #启动爬虫命令：scrapy crawl ark
 #输出结果：scrapy crawl ark -o ark.csv
